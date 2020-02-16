b48a7ed2caac5f774316208abbed9b5748ef7603
slavon1265 <60267157+slavon1265@users.noreply.github.com>
2020-02-16 02:02:51 +0200

    v1

---
+++ 03.02.2020/scripts/9-script.js
@@ -0,0 +1,15 @@
+/*
+Найдите и исправьте ошибки в коде:
+
+for (let i = 0; i < 10; i++) {
+	for (let j = 0; j < 10; i++) {
+		console.log(i + j);
+	}
+}
+
+*/
+for (let i = 0; i < 10; i++) {
+	for (let j = 0; j < 10; j++) {
+		console.log(i + j);
+	}
+}
